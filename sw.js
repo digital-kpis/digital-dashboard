05e048cd821dd5147aac9e752db7f1f203f0e375
ddsouza@2026 <diegolibri@hotmail.com>
2026-06-04 20:33:32 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'ifood-v2';
+const ASSETS = [
+  '/digital-dashboard/',
+  '/digital-dashboard/index.html',
+  '/digital-dashboard/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).then(resp => {
+        const clone = resp.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return resp;
+      }).catch(() => cached);
+    })
+  );
+});
